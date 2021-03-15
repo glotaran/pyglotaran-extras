c314b3c013c3ed56467db4abf418158b02153bbd
Joris Snellenburg <jsnel@users.noreply.github.com>
2021-03-15 01:53:37 +0100

    Fix plot_svd
    Fix indexing of indices in plot_svd
    Closes #2

--- pyglotaran_extras/plotting/plot_svd.py
+++ pyglotaran_extras/plotting/plot_svd.py
@@ -15,7 +15,9 @@ def plot_svd(res, axes, linlog=False, linthresh=1):
 def plot_lsv_data(res, ax, indices=range(4), linlog=False, linthresh=1):
     """ Plot left singular vectors (time) of the data matrix """
     dLSV = res.data_left_singular_vectors
-    dLSV.isel(left_singular_value_index=indices[: len(dLSV)]).plot.line(x="time", ax=ax)
+    dLSV.isel(
+        left_singular_value_index=indices[: len(dLSV.left_singular_value_index)]
+    ).plot.line(x="time", ax=ax)
     ax.set_title("data. LSV")
     if linlog:
         ax.set_xscale("symlog", linthresh=linthresh)
@@ -24,16 +26,16 @@ def plot_lsv_data(res, ax, indices=range(4), linlog=False, linthresh=1):
 def plot_rsv_data(res, ax, indices=range(4)):
     """ Plot right singular vectors (spectra) of the data matrix """
     dRSV = res.data_right_singular_vectors
-    dRSV.isel(right_singular_value_index=indices[: len(dRSV)]).plot.line(
-        x="spectral", ax=ax
-    )
+    dRSV.isel(
+        right_singular_value_index=indices[: len(dRSV.right_singular_value_index)]
+    ).plot.line(x="spectral", ax=ax)
     ax.set_title("data. RSV")
 
 
 def plot_sv_data(res, ax, indices=range(10)):
     """ Plot singular values of the data matrix """
     dSV = res.data_singular_values
-    dSV.sel(singular_value_index=indices[: len(dSV)]).plot.line(
+    dSV.sel(singular_value_index=indices[: len(dSV.singular_value_index)]).plot.line(
         "ro-", yscale="log", ax=ax
     )
     ax.set_title("data. log(SV)")
@@ -47,7 +49,9 @@ def plot_lsv_residual(
         rLSV = res.weighted_residual_left_singular_vectors
     else:
         rLSV = res.residual_left_singular_vectors
-    rLSV.isel(left_singular_value_index=indices[: len(rLSV)]).plot.line(x="time", ax=ax)
+    rLSV.isel(
+        left_singular_value_index=indices[: len(rLSV.left_singular_value_index)]
+    ).plot.line(x="time", ax=ax)
     ax.set_title("res. LSV")
     if linlog:
         ax.set_xscale("symlog", linthresh=linthresh)
@@ -59,9 +63,9 @@ def plot_rsv_residual(res, ax, indices=range(2)):
         rRSV = res.weighted_residual_right_singular_vectors
     else:
         rRSV = res.residual_right_singular_vectors
-    rRSV.isel(right_singular_value_index=indices[: len(rRSV)]).plot.line(
-        x="spectral", ax=ax
-    )
+    rRSV.isel(
+        right_singular_value_index=indices[: len(rRSV.right_singular_value_index)]
+    ).plot.line(x="spectral", ax=ax)
     ax.set_title("res. RSV")
 
 
@@ -71,7 +75,7 @@ def plot_sv_residual(res, ax, indices=range(10)):
         rSV = res.weighted_residual_singular_values
     else:
         rSV = res.residual_singular_values
-    rSV.sel(singular_value_index=indices[: len(rSV)]).plot.line(
+    rSV.sel(singular_value_index=indices[: len(rSV.singular_value_index)]).plot.line(
         "ro-", yscale="log", ax=ax
     )
     ax.set_title("res. log(SV)")
