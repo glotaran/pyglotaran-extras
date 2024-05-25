14af4ade4ae1e106ffab4a51ca34f4ae437e2c1b
s-weigand <s.weigand.phy@gmail.com>
2024-05-25 20:29:21 +0200

    ✨Add PerFunctionPlotConfig

--- pyglotaran_extras/config/plot_config.py
+++ pyglotaran_extras/config/plot_config.py
@@ -2,11 +2,14 @@
 
 from __future__ import annotations
 
+from collections.abc import Iterable
 from collections.abc import Iterator
 from collections.abc import Mapping
 from typing import Any
 from typing import Literal
 
+import numpy as np
+from matplotlib.axes import Axes
 from pydantic import BaseModel
 from pydantic import ConfigDict
 from pydantic import Field
@@ -68,12 +71,10 @@ class PlotLabelOverRideMap(RootModel, Mapping):
             return {}
         parsed_values: dict[str, PlotLabelOverRideValue] = {}
         for key, value in values.items():
-            if isinstance(value, PlotLabelOverRideValue):
-                parsed_values[key] = value
-            elif isinstance(value, str):
+            if isinstance(value, str):
                 parsed_values[key] = PlotLabelOverRideValue(target_name=value)
             else:
-                parsed_values[key] = PlotLabelOverRideValue(**value)
+                parsed_values[key] = PlotLabelOverRideValue.model_validate(value)
         return parsed_values
 
     def __iter__(self) -> Iterator[str]:  # noqa: DOC
@@ -87,3 +88,90 @@ class PlotLabelOverRideMap(RootModel, Mapping):
     def __getitem__(self, item_label: str) -> PlotLabelOverRideValue:  # noqa: DOC
         """Access items."""
         return self.root[item_label]
+
+
+class PerFunctionPlotConfig(BaseModel):
+    """Per function plot configuration."""
+
+    model_config = ConfigDict(extra="forbid")
+
+    default_args_override: dict[str, Any] = Field(
+        default_factory=dict,
+        description="Default arguments to use if not specified in function call.",
+    )
+    axis_label_override: PlotLabelOverRideMap = Field(default_factory=PlotLabelOverRideMap)
+
+    def merge(self, other: PerFunctionPlotConfig) -> PerFunctionPlotConfig:
+        """Merge two ``PerFunctionPlotConfig``'s where ``other`` overrides values.
+
+        Parameters
+        ----------
+        other : PerFunctionPlotConfig
+            Other ``PerFunctionPlotConfig`` to merge in.
+
+        Returns
+        -------
+        PerFunctionPlotConfig
+        """
+        self_dict = self.model_dump()
+        other_dict = other.model_dump()
+        return PerFunctionPlotConfig(
+            default_args_override=(
+                self_dict["default_args_override"] | other_dict["default_args_override"]
+            ),
+            axis_label_override=(
+                self_dict["axis_label_override"] | other_dict["axis_label_override"]
+            ),
+        )
+
+    def find_override_kwargs(self, not_user_provided_kwargs: set[str]) -> dict[str, Any]:
+        """Config key word arguments that were not provided by the user and are safe to override.
+
+        Parameters
+        ----------
+        not_user_provided_kwargs : set[str]
+            Set of keyword arguments that were provided by the user and thus should not be
+            overridden.
+
+        Returns
+        -------
+        dict[str, Any]
+        """
+        return {
+            k: self.default_args_override[k]
+            for k in self.default_args_override
+            if k in not_user_provided_kwargs
+        }
+
+    def update_axes_labels(
+        self, axes: Axes | Iterable[Axes] | np.ndarray[Axes, np.dtype[Any]] | None
+    ) -> None:
+        """Apply label overrides to ``axes``.
+
+        Parameters
+        ----------
+        axes : Axes | Iterable[Axes] | np.ndarray[Axes, np.dtype[Any]] | None
+            Axes to apply the override to.
+        """
+        if axes is None:
+            return
+        if isinstance(axes, Axes):
+            orig_x_label = axes.get_xlabel()
+            orig_y_label = axes.get_ylabel()
+
+            if orig_x_label in self.axis_label_override and (
+                override_item := self.axis_label_override[orig_x_label]
+            ).axis in ("x", "both"):
+                axes.set_xlabel(override_item.target_name)
+
+            if orig_y_label in self.axis_label_override and (
+                override_item := self.axis_label_override[orig_y_label]
+            ).axis in ("y", "both"):
+                axes.set_ylabel(override_item.target_name)
+
+        elif isinstance(axes, np.ndarray):
+            for ax in axes.flatten():
+                self.update_axes_labels(ax)
+        else:
+            for ax in axes:
+                self.update_axes_labels(ax)
--- tests/config/test_plot_config.py
+++ tests/config/test_plot_config.py
@@ -6,12 +6,14 @@ from io import StringIO
 from textwrap import dedent
 from typing import Any
 
+import matplotlib.pyplot as plt
 import pytest
 from jsonschema import ValidationError as SchemaValidationError
 from jsonschema import validate
 from pydantic import ValidationError as PydanticValidationError
 from ruamel.yaml import YAML
 
+from pyglotaran_extras.config.plot_config import PerFunctionPlotConfig
 from pyglotaran_extras.config.plot_config import PlotLabelOverRideMap
 from pyglotaran_extras.config.plot_config import PlotLabelOverRideValue
 
@@ -70,9 +72,172 @@ def test_plot_label_over_ride_map():
 
     assert PlotLabelOverRideMap().model_dump() == {}
     with pytest.raises(PydanticValidationError) as execinfo:
-        PlotLabelOverRideMap({"invalid": {"invalid": 1}})
+        PlotLabelOverRideMap.model_validate({"invalid": {"invalid": 1}})
 
     assert (
         "target_name\n  Field required [type=missing, input_value={'invalid': 1}, input_type=dict]"
         in str(execinfo.value)
     )
+
+
+def test_per_function_plot_config():
+    """Initialize with correct defaults and validate correctly."""
+    function_config_data: dict[str, Any] = YAML().load(
+        StringIO(
+            dedent(
+                """
+                default_args_override:
+                    test_arg: true
+                axis_label_override:
+                    "Old Label": "New Label"
+                """
+            )
+        )
+    )
+    function_config = PerFunctionPlotConfig.model_validate(function_config_data)
+
+    validate(instance=function_config_data, schema=PerFunctionPlotConfig.model_json_schema())
+
+    assert function_config.default_args_override["test_arg"] is True
+    assert function_config.axis_label_override["Old Label"] == PlotLabelOverRideValue(
+        target_name="New Label"
+    )
+
+    with pytest.raises(SchemaValidationError) as execinfo:
+        validate(
+            instance={"unknown": 1},
+            schema=PerFunctionPlotConfig.model_json_schema(),
+        )
+
+    assert str(execinfo.value).startswith(
+        "Additional properties are not allowed ('unknown' was unexpected)"
+    )
+
+    assert PerFunctionPlotConfig().model_dump() == {
+        "default_args_override": {},
+        "axis_label_override": {},
+    }
+    with pytest.raises(PydanticValidationError) as execinfo:
+        PerFunctionPlotConfig.model_validate({"unknown": 1})
+
+    assert (
+        "1 validation error for PerFunctionPlotConfig\n"
+        "unknown\n"
+        "  Extra inputs are not permitted [type=extra_forbidden, input_value=1, input_type=int]"
+        in str(execinfo.value)
+    )
+
+
+def test_per_function_plot_config_merge():
+    """Values with same key get updated and other values stay the same."""
+    original_config = PerFunctionPlotConfig(
+        default_args_override={"test_arg": "to_be_changed", "not_updated": "same"},
+        axis_label_override={"Old Label": "to_be_changed", "not_updated": "same"},
+    )
+    update_config = PerFunctionPlotConfig(
+        default_args_override={"test_arg": "changed"},
+        axis_label_override={"Old Label": "changed"},
+    )
+
+    merged_config = original_config.merge(update_config)
+
+    assert merged_config.default_args_override["test_arg"] == "changed"
+    assert merged_config.default_args_override["not_updated"] == "same"
+    assert merged_config.axis_label_override["Old Label"] == PlotLabelOverRideValue(
+        target_name="changed"
+    )
+    assert merged_config.axis_label_override["not_updated"] == PlotLabelOverRideValue(
+        target_name="same"
+    )
+
+
+def test_per_function_plot_find_override_kwargs():
+    """Only get kwargs that were not provided by the user and are known."""
+    original_config = PerFunctionPlotConfig(
+        default_args_override={"test_arg": "to_be_changed", "not_updated": "same"},
+    )
+
+    no_override_kwargs = original_config.find_override_kwargs(set())
+    assert no_override_kwargs == {}
+
+    override_kwargs = original_config.find_override_kwargs({"not_updated", "unknown_arg"})
+    assert override_kwargs == {"not_updated": "same"}
+
+
+def test_per_function_plot_update_axes_labels():
+    """Only labels where the axis and current label match get updated."""
+
+    def create_test_ax():
+        ax = plt.subplot()
+        ax.set_xlabel("x")
+        ax.set_ylabel("y")
+        return ax
+
+    simple_config = PerFunctionPlotConfig(axis_label_override={"x": "new x", "y": "new y"})
+
+    simple_config.update_axes_labels(None)
+
+    ax_both = create_test_ax()
+    simple_config.update_axes_labels(ax_both)
+
+    assert ax_both.get_xlabel() == "new x"
+    assert ax_both.get_ylabel() == "new y"
+
+    ax_explicit = create_test_ax()
+
+    PerFunctionPlotConfig(
+        axis_label_override=PlotLabelOverRideMap(
+            {
+                "x": PlotLabelOverRideValue(target_name="new x", axis="x"),
+                "y": PlotLabelOverRideValue(target_name="new y", axis="y"),
+            }
+        )
+    ).update_axes_labels(ax_explicit)
+    assert ax_explicit.get_xlabel() == "new x"
+    assert ax_explicit.get_ylabel() == "new y"
+
+    ax_mismatch = create_test_ax()
+
+    PerFunctionPlotConfig(
+        axis_label_override=PlotLabelOverRideMap(
+            {
+                "x": PlotLabelOverRideValue(target_name="new x", axis="y"),
+                "y": PlotLabelOverRideValue(target_name="new y", axis="x"),
+            }
+        )
+    ).update_axes_labels(ax_mismatch)
+    assert ax_mismatch.get_xlabel() == "x"
+    assert ax_mismatch.get_ylabel() == "y"
+
+    _, np_axes = plt.subplots(1, 2)
+
+    assert np_axes.shape == (2,)
+
+    np_axes[0].set_xlabel("x")
+    np_axes[0].set_ylabel("y-keep")
+    np_axes[1].set_xlabel("x-keep")
+    np_axes[1].set_ylabel("y")
+
+    simple_config.update_axes_labels(np_axes)
+
+    assert np_axes[0].get_xlabel() == "new x"
+    assert np_axes[0].get_ylabel() == "y-keep"
+    assert np_axes[1].get_xlabel() == "x-keep"
+    assert np_axes[1].get_ylabel() == "new y"
+
+    _, ax0 = plt.subplots(1, 1)
+    _, ax1 = plt.subplots(1, 1)
+
+    iterable_axes = (ax0, ax1)
+
+    iterable_axes[0].set_xlabel("x")
+    iterable_axes[0].set_ylabel("y-keep")
+    iterable_axes[1].set_xlabel("x-keep")
+    iterable_axes[1].set_ylabel("y")
+
+    simple_config.update_axes_labels(iterable_axes)
+
+    assert iterable_axes[0].get_xlabel() == "new x"
+    assert iterable_axes[0].get_ylabel() == "y-keep"
+    assert iterable_axes[1].get_xlabel() == "x-keep"
+    assert iterable_axes[1].get_ylabel() == "new y"
