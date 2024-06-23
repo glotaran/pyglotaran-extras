077f853bd7f7134e1cfbcead6d474ca561241f08
s-weigand <s.weigand.phy@gmail.com>
2024-06-23 15:01:29 +0200

    👌 Make config loading more resiliant against validation errors
    👌 Improve error collection and messages

--- tests/config/test_config.py
+++ tests/config/test_config.py
@@ -7,8 +7,10 @@ import subprocess
 import sys
 from pathlib import Path
 from shutil import copyfile
+from textwrap import dedent
 
 import pytest
+from pydantic import __version__ as pydantic_version
 from ruamel.yaml import YAML
 
 from pyglotaran_extras.config.config import CONFIG_FILE_STEM
@@ -233,7 +235,7 @@ def test_merge_configs():
     Since we already tested all permutations for 2 configs in ``test_config_merge``
     this test can fucus on the case with more than 2 configs.
     """
-    assert merge_configs([]) is None
+    assert merge_configs([]) == Config()
 
     original_values, update_values, expected_values = tuple(
         YAML().load_all(TEST_DATA / "config/config_merge.yml")
@@ -385,3 +387,45 @@ def test_load_config_on_import_none_root_import(tmp_path: Path, import_load_scri
     subprocess.run([sys.executable, import_load_script])
 
     check_config(tmp_path, dest_path)
+
+
+def test_load_config_on_import_broken_config(tmp_path: Path, import_load_script: Path):
+    """Also works something from non root ``pyglotaran_extras`` was imported first."""
+    src_path = TEST_DATA / "config/broken_config.yml"
+    dest_path = tmp_path / "pyglotaran_extras_config.yml"
+    copyfile(src_path, dest_path)
+
+    res = subprocess.run([sys.executable, import_load_script], text=True, capture_output=True)
+    assert res.returncode == 0
+
+    version = pydantic_version[:-2]
+    assert res.stderr == dedent(
+        f"""\
+        Error loading the config:
+        Source path: {dest_path.as_posix()}
+        Error: 7 validation errors for Config
+        plotting.general.axis_label_override.target_name
+          Field required [type=missing, input_value={{'not_allowed_general': False}}, \
+input_type=CommentedMap]
+            For further information visit https://errors.pydantic.dev/{version}/v/missing
+        plotting.general.axis_label_override.not_allowed_general
+          Extra inputs are not permitted [type=extra_forbidden, input_value=False, input_type=bool]
+            For further information visit https://errors.pydantic.dev/{version}/v/extra_forbidden
+        plotting.general.invalid_kw_general
+          Extra inputs are not permitted [type=extra_forbidden, input_value=True, input_type=bool]
+            For further information visit https://errors.pydantic.dev/{version}/v/extra_forbidden
+        plotting.test_func.axis_label_override.target_name
+          Field required [type=missing, input_value={{'not_allowed_test_func': False}}, \
+input_type=CommentedMap]
+            For further information visit https://errors.pydantic.dev/{version}/v/missing
+        plotting.test_func.axis_label_override.not_allowed_test_func
+          Extra inputs are not permitted [type=extra_forbidden, input_value=False, input_type=bool]
+            For further information visit https://errors.pydantic.dev/{version}/v/extra_forbidden
+        plotting.test_func.invalid_kw_test_func
+          Extra inputs are not permitted [type=extra_forbidden, input_value=True, input_type=bool]
+            For further information visit https://errors.pydantic.dev/{version}/v/extra_forbidden
+        invalid_kw_root
+          Extra inputs are not permitted [type=extra_forbidden, input_value=True, input_type=bool]
+            For further information visit https://errors.pydantic.dev/{version}/v/extra_forbidden
+        """
+    )
